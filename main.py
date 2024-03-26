9fcab87961a4afe167ac9c4a672dfc58a5c32c01
Maalen <malen8622@gmail.com>
2024-03-26 13:18:46 +0700

    Feature_issue4: Add a withdraw function

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ users = {
     "user2": {"password": "5678", "balance": 2000}
 }
 current_user = None
+def withdraw():
+    global users, current_user
+    if current_user:
+        amount = float(input("Enter amount to withdraw: "))
+        if amount <= users[current_user]['balance']:
+            users[current_user]['balance'] -= amount
+            print(f"Withdrew ${amount} successfully.")
+    else:
+        print("Please sign in first.")

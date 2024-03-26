c52b151a4306e568d959d678a15289975fa58dcd
Maalen <malen8622@gmail.com>
2024-03-26 13:44:42 +0700

    Change

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ users = {
     "user2": {"password": "5678", "balance": 2000}
 }
 current_user = None
-def withdraw():
-    global users, current_user
-    if current_user:
-        amount = float(input("Enter amount to withdraw: "))
-        if amount <= users[current_user]['balance']:
-            users[current_user]['balance'] -= amount
-            print(f"Withdrew ${amount} successfully.")
-    else:
-        print("Please sign in first.")
 
 def sign_up():
     global users
@@ -45,7 +36,7 @@ def sign_in():
             print("---Please reenter again---")
             print("==========================")
             print()
-            
+
 def withdraw():
     global users, current_user
     if current_user:

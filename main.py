0d12c193014c08352240552711e1e74aaaf25bd2
AnB School 0027 <rattanakphok9@gmail.com>
2024-03-26 14:13:39 +0700

    feature/issue 6 : resolved Add a loan function #7

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ users = {
     "user2": {"password": "5678", "balance": 2000}
 }
 current_user = None
+
+def loan():
+    if current_user:
+        amount = float(input("Enter loan amount: "))
+        users[current_user]["balance"] += amount
+        print(f"Loan of ${amount} credited to your account. New balance: ${users[current_user]['balance']}")
+    else:
+        print("Please sign in to apply for a loan.")

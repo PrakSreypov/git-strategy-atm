c86171d3ffb1e9a2e0ec32c59c55baf222b988b3
chounan <tch23125@gmail.com>
2024-03-26 13:20:20 +0700

    sign-in and sign-up func feature/issue1

--- main.py
+++ main.py
@@ -3,3 +3,36 @@ users = {
     "user2": {"password": "5678", "balance": 2000}
 }
 current_user = None
+
+def sign_up():
+    global users
+    print("---- create an account ----")
+    username = input("name: ")
+    if username in users:
+        print("Username already exists. Please choose another username.")
+        return
+    password = input("Set password: ")
+    #balance = float(input("Enter initial balance: "))
+    users[username] = {"password": password, "balance": 0}
+    current_user = username
+    print("Account created successfuly.")
+    print()
+
+def sign_in():
+    global current_user, users
+
+    print("    ---- Sign in ----") 
+    while True :
+        username = input("Please Enter your Username: ")
+        password = input("Enter Password: ")
+        print()
+        if username in users and users[username]["password"] == password:
+            current_user = username
+            print(f"Welcome  {username}  ")
+            break
+                
+        else:
+            print("Invalid user name or password")
+            print("---Please reenter again---")
+            print("==========================")
+            print()

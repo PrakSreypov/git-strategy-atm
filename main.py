bc8b98c6a83c48504900b840666a04f3c2400a3d
AnB School 0033 <anbschool0033@AnBs-Mac-mini-65.local>
2024-03-26 13:22:29 +0700

    feature/issue5: Added transfer func

--- main.py
+++ main.py
@@ -3,3 +3,17 @@ users = {
     "user2": {"password": "5678", "balance": 2000}
 }
 current_user = None
+def transfer():
+    global users, current_user
+    if current_user:
+        recipient = input("Enter recipient's username: ")
+        if recipient in users:
+            amount = float(input("Enter amount to transfer: "))
+            if amount <= users[current_user]['balance']:
+                users[current_user]['balance'] -= amount
+                users[recipient]['balance'] += amount
+                print(f"Transferred ${amount} to {recipient} successfully.")
+        else:
+            print("Recipient not found.")
+    else:
+        print("Please sign in first.")

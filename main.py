25face024ce974cb09dd8ddebece47ad2a1e052c
Lyhua-tech <slyhour69@gmail.com>
2024-03-26 13:58:53 +0700

    feature/issue3: Add a deposit function

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ users = {
     "user2": {"password": "5678", "balance": 2000}
 }
 current_user = None
+def deposit():
+    global users, current_user
+    if current_user:
+        amount = float(input("Enter amount to deposit: "))
+        users[current_user]['balance'] += amount
+        print(f"Deposited successfully. New balance: ${users[current_user]['balance']}")
+    else:
+        print("Please sign in first.")
